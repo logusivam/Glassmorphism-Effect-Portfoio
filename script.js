b48af15bd75e5e417fd88e5397203c53ceb72317
Loganathan G P <logusivam26@gmail.com>
2024-08-04 10:52:57 +0000

    Aug 4, 2024, 4:22 PM

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const black_theme = document.querySelector(".black-theme");
 const change = document.querySelector("#switch");
 
 change.addEventListener("click", changeTheme);
+
+function changeTheme()
+{
+    if(document.body.classList.contains("black-theme"))
+    {
+        document.body.classList
+    }
+}
