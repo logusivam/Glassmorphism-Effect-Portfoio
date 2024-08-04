b8e7584c0461e656b07d7add5ff5287d07837c3f
Loganathan G P <logusivam26@gmail.com>
2024-08-04 11:21:08 +0000

    Aug 4, 2024, 4:51 PM

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const black_theme = document.querySelector(".black-theme");
+const black_theme = document.querySelector('.black-theme');
 
-const change = document.querySelector("#switch");
+const change = document.querySelector('#switch');
 
-change.addEventListener("click", changeTheme);
+change.addEventListener('click', changeTheme);
 
 function changeTheme()
 {
-    if(document.body.classList.contains("black-theme"))
+    if(document.body.classList.contains('black-theme'))
     {
-        document.body.classList.remove("black-theme");
-        document.body.classList.add("white-theme");
+        document.body.classList.remove('black-theme');
+        document.body.classList.add('white-theme');
         change.innerHTML = '<i class="fas fa-moon"></i>';
     }
 
     else
     {
-        document.body.classList.remove("white-theme");
-        document.body.classList.add("black-theme");
+        document.body.classList.remove('white-theme');
+        document.body.classList.add('black-theme');
         change.innerHTML = '<i class="fas fa-sun"></i>';
     }
 }
